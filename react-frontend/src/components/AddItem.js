12b99ee01d03b0c5ec72a38f9d058f46e5b3f655
Chris Li <hychrisli@users.noreply.github.com>
2018-02-21 09:56:16 -0800

    react restful API express

---
+++ react-frontend/src/components/AddItem.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class AddItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value:''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert(this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Add Iem:
+                        <input type="text" value={this.state.value}
+                               onChange={this.handleChange}
+                               className="form-control"/>
+                    </label><br/>
+                    <input type="submit" value="Submit" ClassName="btn btn-primary"/>
+                </form>
+            </div>);
+    }
+
+}
+
+export default AddItem;
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+
+import App from './App';
+import AddItem from './components/AddItem';
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path='/' component={App}/>
+            <Route path='/add-item' component={AddItem}/>
         </div>
     </Router>,
     document.getElementById('root'));
